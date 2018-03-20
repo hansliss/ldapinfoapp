b326101e13c2c7d0d9752775e132cd353b9c4dae
hansliss <Hans@Liss.nu>
2018-03-20 13:46:06 +0100

    Added userid lookup

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $h->emit();
 ?>
 <table>
     <tr><td colspan="2"><a href="user.php">Start with yourself (<?php print $_SERVER["REMOTE_USER"]; ?>)</a></td></tr>
+    <tr><td>Look up by Akka ID</td><td><form class="compact" action="user.php" method="GET"><input type="text" name="username"><input type="submit" value="Go"></form></td></tr>
     <tr><td>Search for a user</td><td><input type="text" name="username" onkeyup="doDelayedSearch('user', 'userbox', this)"  autocomplete="off" />
     <br/><div id="userbox"></div></td></tr>
     <tr><td>Search for a group</td><td><input type="text" name="groupDN" onkeyup="doDelayedSearch('group', 'groupbox', this)"  autocomplete="off" />
